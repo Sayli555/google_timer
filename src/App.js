f8619848a5884f4853f0f933d1c0a026678213c3
Sayli555 <96167212+Sayli555@users.noreply.github.com>
2022-04-21 23:48:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [time,setTime]=useState(0);
+  const [timeron,setTimon]=useState(false);
+
+  useEffect(()=>{
+    let interval=null;
+    if(timeron){
+      interval=setInterval(()=>{
+        setTime(pr=>pr+10)
+      },10)
+    }
+    else{
+      clearInterval(interval)
+    }
+    return ()=>clearInterval(interval)
+  },[timeron])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+       <span>{("0" +Math.floor((time/60000)%60)).slice(-2)}:</span>
+       <span>{("0" +Math.floor((time/1000)%60)).slice(-2)}:</span>
+       <span>{("0" +((time/10)%100)).slice(-2)}</span>
+     </div>
+     <div>
+       {/* <button onClick={()=>setTimon(true)}>Start</button>
+       <button onClick={()=>setTimon(false)}>Stop</button>
+       <button onClick={()=>setTimon(true)}>Resume</button>
+       <button onClick={()=>setTime(0)}>Reset</button> */}
+
+       {!timeron && time===0 && (
+         <button onClick={()=>setTimon(true)}>Start</button>
+       )}
+        {timeron && (
+         <button onClick={()=>setTimon(false)}>Stop</button>
+        )}
+        {!timeron && time!==0 &&(
+         <button onClick={()=>setTimon(true)}>Resume</button>
+         ) }
+        {!timeron && time>0  &&(
+         <button onClick={()=>setTime(0)}>Reset</button>
+        )}
+       
+       
+     </div>
     </div>
   );
 }
